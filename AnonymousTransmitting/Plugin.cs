c4e4cecd0b03a2d451d7cd7eee99b9bc1dde70ca
Banalny_Banan <elisey1324@gmail.com>
2024-02-20 22:31:35 +0200

    Now uses Loader.Plugins.Any to check for XPSystem

--- AnonymousTransmitting/Plugin.cs
+++ AnonymousTransmitting/Plugin.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 using Exiled.API.Enums;
 using Exiled.API.Features.Items;
 using Exiled.Events.EventArgs.Interfaces;
+using Exiled.Loader;
 using MEC;
 using Handlers = Exiled.Events.Handlers;
 
@@ -17,7 +18,7 @@ public class Plugin : Plugin<Config>
     public override string Prefix => "AnonymousTransmitting";
     public override string Name => Prefix;
     public override string Author => "Banalny_Banan";
-    public override Version Version { get; } = new(1, 2, 0);
+    public override Version Version { get; } = new(1, 2, 1);
     public override Version RequiredExiledVersion { get; } = new(8, 8, 0);
 
     private bool xpSystemPresent = false;
@@ -28,7 +29,7 @@ public class Plugin : Plugin<Config>
             Handlers.Player.Transmitting += OnTransmitting;
         if (Config.AnonymizeIntercom)
             Handlers.Player.IntercomSpeaking += OnTransmitting;
-        if(File.Exists(Path.Combine(Paths.Plugins, "XPSystem-EXILED.dll")))
+        if(Loader.Plugins.Any(plg => plg.Name == "XPSystem"))
             xpSystemPresent = true;
         base.OnEnabled();
     }
